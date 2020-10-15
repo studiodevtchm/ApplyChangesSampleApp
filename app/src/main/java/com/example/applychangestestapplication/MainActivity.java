e3faaada3c03f68835c99a55aef38fae35943923
smogathapalli <mskchakravarthi@gmail.com>
2020-10-15 15:11:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/applychangestestapplication/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.applychangestestapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.applychangestestapplication.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, fetchMessage(),Snackbar.LENGTH_LONG).setAction("Action",null).show();
+            }
+        });
+
+        /* Apply Code changes
+            1. Here we can change integer value of 'code' and click on Apply Code Change button (or) Ctrl+Alt+F10.
+            2. Check 'apply code changes' success message popup and show in the Run tab.
+         */
+        int code = 123;
+    }
+
+    /*  Apply changes
+         1. Here we can change value of 'message' in the string resource file and click on Apply Change button (or) Ctrl+F10.
+         2. Check 'apply changes' success message popup and show in the Run tab.
+         3. Click on Floating Action button in the emulator and see if changed message is appearing.
+     */
+    public  String fetchMessage() {
+        return getString(R.string.message);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
